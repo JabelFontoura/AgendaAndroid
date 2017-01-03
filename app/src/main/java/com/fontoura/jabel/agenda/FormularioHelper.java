79ab34bbed0680df209a456be57d1a4f93b4676c
Jabel Santos Fontoura <0148111@adm.feevale.br>
2017-01-03 15:05:13 -0200

    first commit

---
+++ app/src/main/java/com/fontoura/jabel/agenda/FormularioHelper.java
@@ -0,0 +1,70 @@
+package com.fontoura.jabel.agenda;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.RatingBar;
+
+import com.fontoura.jabel.agenda.model.Aluno;
+
+/**
+ * Created by Jabel on 12/10/2016.
+ */
+
+public class FormularioHelper {
+
+    private final EditText campoNome;
+    private final EditText campoEndereco;
+    private final EditText campoTelefone;
+    private final EditText campoSite;
+    private final RatingBar campoNota;
+    private final ImageView campoFoto;
+    private Aluno aluno;
+
+    public FormularioHelper(FormularioActivity activity){
+        aluno = new Aluno();
+
+        campoNome = (EditText) activity.findViewById(R.id.formulario_nome);
+        campoEndereco = (EditText) activity.findViewById(R.id.formulario_endereco);
+        campoTelefone = (EditText) activity.findViewById(R.id.formulario_telefone);
+        campoSite = (EditText) activity.findViewById(R.id.formulario_site);
+        campoNota = (RatingBar) activity.findViewById(R.id.formulario_nota);
+        campoFoto = (ImageView) activity.findViewById(R.id.formulario_foto);
+    }
+
+    public Aluno pegaAluno() {
+        aluno.setNome(campoNome.getText().toString());
+        aluno.setEndereco(campoEndereco.getText().toString());
+        aluno.setTelefone(campoTelefone.getText().toString());
+        aluno.setSite(campoSite.getText().toString());
+        aluno.setNota(Double.valueOf(campoNota.getProgress()));
+        if(campoFoto.getTag() != null) aluno.setCaminhoFoto(campoFoto.getTag().toString());
+
+        return aluno;
+    }
+
+    public void preencheFormulario(Aluno aluno) {
+        campoNome.setText(aluno.getNome());
+        campoEndereco.setText(aluno.getEndereco());
+        campoTelefone.setText(aluno.getTelefone());
+        campoSite.setText(aluno.getSite());
+        campoNota.setProgress(aluno.getNota().intValue());
+        carregaImagem(aluno.getCaminhoFoto());
+
+        this.aluno = aluno;
+    }
+
+    public void carregaImagem(String caminhoFoto) {
+        if(caminhoFoto != null) {
+            Log.d("Foto", caminhoFoto);
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
+            campoFoto.setImageBitmap(bitmapReduzido);
+            campoFoto.setScaleType(ScaleType.FIT_XY);
+            campoFoto.setTag(caminhoFoto);
+        }
+    }
+}
---
+++ app/src/main/java/com/fontoura/jabel/agenda/ListaAlunosActivity.java
@@ -0,0 +1,159 @@
+package com.fontoura.jabel.agenda;
+
+import android.Manifest.permission;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.fontoura.jabel.agenda.adapter.AlunosAdapter;
+import com.fontoura.jabel.agenda.dao.AlunoDAO;
+import com.fontoura.jabel.agenda.model.Aluno;
+import com.fontoura.jabel.agenda.task.EnviaAlunosTask;
+
+import java.util.List;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    private ListView listaAlunos;
+    private AlunoDAO dao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+        listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+
+        listaAlunos.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> list, View item, int position, long id) {
+                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(position);
+
+                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                intentVaiProFormulario.putExtra("aluno", aluno);
+                startActivity(intentVaiProFormulario);
+            }
+        });
+
+        Button novoAluno = (Button) findViewById(R.id.novo_aluno);
+
+        novoAluno.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intentVaiProFormulario);
+            }
+        });
+
+        registerForContextMenu(listaAlunos);
+    }
+
+    private void carregaLista() {
+        dao = new AlunoDAO(this);
+
+        List<Aluno> alunos = dao.buscaAlunos();
+        dao.close();
+
+        AlunosAdapter adapter = new AlunosAdapter(this, alunos);
+
+        listaAlunos.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        carregaLista();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_lista_alunos, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_enviar_notas:
+                new EnviaAlunosTask(this).execute();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Cria menus de contexto com clique longo no aluno
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenuInfo menuInfo) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterContextMenuInfo) menuInfo;
+        final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+        MenuItem itemSMS = menu.add("Enviar SMS");
+        Intent intentSMS = new Intent(Intent.ACTION_VIEW);
+        intentSMS.setData(Uri.parse("sms:" + aluno.getTelefone()));
+        itemSMS.setIntent(intentSMS);
+
+        MenuItem itemMapa = menu.add("Visuzalir no mapa");
+        Intent intentMapa = new Intent(Intent.ACTION_VIEW);
+        intentMapa.setData(Uri.parse("geo:0,0?q=" + aluno.getEndereco()));
+        itemMapa.setIntent(intentMapa);
+
+        MenuItem itemSite = menu.add("Visitar site");
+        Intent intentSite = new Intent(Intent.ACTION_VIEW);
+
+        MenuItem itemLigar = menu.add("Ligar");
+        itemLigar.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this, new String[]{permission.CALL_PHONE}, 123);
+                }else {
+
+                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:" + aluno.getTelefone()));
+
+                    startActivity(intentLigar);
+                }
+                return false;
+            }
+        });
+
+        String site = aluno.getSite();
+        if(!site.startsWith("http://")) site = "http://" + site;
+
+        intentSite.setData(Uri.parse(site));
+        itemSite.setIntent(intentSite);
+
+        MenuItem deletar = menu.add("Deletar");
+        deletar.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                dao = new AlunoDAO(ListaAlunosActivity.this);
+
+                dao.deleta(aluno);
+                dao.close();
+
+                carregaLista();
+
+                return false;
+            }
+        });
+
+    }
+}
