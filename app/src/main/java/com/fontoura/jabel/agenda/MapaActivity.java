2f8f69376ef4444528bf36f92d014c33e9890d05
Jabel Santos Fontoura <0148111@adm.feevale.br>
2017-01-06 14:36:49 -0200

    Melhorias no mapa

--- app/src/main/java/com/fontoura/jabel/agenda/MapaActivity.java
+++ app/src/main/java/com/fontoura/jabel/agenda/MapaActivity.java
@@ -1,5 +1,10 @@
 package com.fontoura.jabel.agenda;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -9,16 +14,37 @@ import com.google.android.gms.maps.SupportMapFragment;
 
 public class MapaActivity extends AppCompatActivity {
 
+    private static final int REQUEST_PERMISSOES = 1;
+    private MapaFragment mapaFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
 
-        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
-        FragmentTransaction tx = manager.beginTransaction();
+        mapaFragment = new MapaFragment();
 
-        tx.replace(R.id.frame_mapa, new SupportMapFragment());
-        tx.commit();
+        transaction.replace(R.id.frame_mapa, mapaFragment);
+        transaction.commit();
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                String[] permissoes = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+                requestPermissions(permissoes, REQUEST_PERMISSOES);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSOES) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                new Localizador(this, mapaFragment);
+            }
+        }
     }
 }
